2f39ce98600c2eaa94f14684e4ee932ce428aca1
Kai Förstermann <infokaifoerstermann.de>
2020-04-06 15:00:16 +0200

    GUI-Elemente platziert

--- BMI-Rechner/ViewController.designer.cs
+++ BMI-Rechner/ViewController.designer.cs
@@ -14,5 +14,88 @@ namespace BMI_Rechner
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnSubmit { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblFemale { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblHeight { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblMale { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblOutput { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblTitle { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblWeight { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sldrHeight { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sldrWeight { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnSubmit != null) {
+                btnSubmit.Dispose ();
+                btnSubmit = null;
+            }
+
+            if (lblFemale != null) {
+                lblFemale.Dispose ();
+                lblFemale = null;
+            }
+
+            if (lblHeight != null) {
+                lblHeight.Dispose ();
+                lblHeight = null;
+            }
+
+            if (lblMale != null) {
+                lblMale.Dispose ();
+                lblMale = null;
+            }
+
+            if (lblOutput != null) {
+                lblOutput.Dispose ();
+                lblOutput = null;
+            }
+
+            if (lblTitle != null) {
+                lblTitle.Dispose ();
+                lblTitle = null;
+            }
+
+            if (lblWeight != null) {
+                lblWeight.Dispose ();
+                lblWeight = null;
+            }
+
+            if (sldrHeight != null) {
+                sldrHeight.Dispose ();
+                sldrHeight = null;
+            }
+
+            if (sldrWeight != null) {
+                sldrWeight.Dispose ();
+                sldrWeight = null;
+            }
+        }
     }
 }
