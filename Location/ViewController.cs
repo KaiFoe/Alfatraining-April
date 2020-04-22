b11466a8bc041b98bd31fd10ae874734f0c269c0
Kai Förstermann <infokaifoerstermann.de>
2020-04-22 15:37:26 +0200

    Location hinzugefügt

---
+++ Location/ViewController.cs
@@ -0,0 +1,63 @@
+﻿using CoreLocation;
+using Foundation;
+using System;
+using UIKit;
+
+namespace Location
+{
+    public partial class ViewController : UIViewController
+    {
+
+        public static double AccuracyHundredMeters { get; }
+        private CLLocationManager locationManager;
+        private CLLocation location;
+
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            #region Location ausgeben (Text)
+            locationManager = new CLLocationManager();
+
+            locationManager.RequestAlwaysAuthorization();
+
+            locationManager.LocationsUpdated += LocationManager_LocationsUpdated;
+
+            btnStart.TouchUpInside += BtnStart_TouchUpInside;
+            btnEnd.TouchUpInside += BtnEnd_TouchUpInside;
+            #endregion
+        }
+
+        private void LocationManager_LocationsUpdated(object sender, CLLocationsUpdatedEventArgs args)
+        {
+            location = args.Locations[0];
+            double latitude = Math.Round(location.Coordinate.Latitude, 4);
+            double longitude = Math.Round(location.Coordinate.Longitude, 4);
+            double accuracy = Math.Round(location.HorizontalAccuracy, 0);
+
+            lblOutput.Text = string.Format("Latitude: {0}\nLongitude: {1}\nAccuracy: {2}m", latitude, longitude, accuracy);
+        }
+
+        private void BtnEnd_TouchUpInside(object sender, EventArgs e)
+        {
+            locationManager.StopUpdatingLocation();
+            lblOutput.Text = "Standortbestimmung beendet...";
+        }
+
+        private void BtnStart_TouchUpInside(object sender, EventArgs e)
+        {
+            lblOutput.Text = "Bestimme Standort...";
+            locationManager.StartUpdatingLocation();
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ Location/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Location
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnEnd { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnStart { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblOutput { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblTitle { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnEnd != null) {
+                btnEnd.Dispose ();
+                btnEnd = null;
+            }
+
+            if (btnStart != null) {
+                btnStart.Dispose ();
+                btnStart = null;
+            }
+
+            if (lblOutput != null) {
+                lblOutput.Dispose ();
+                lblOutput = null;
+            }
+
+            if (lblTitle != null) {
+                lblTitle.Dispose ();
+                lblTitle = null;
+            }
+        }
+    }
+}
